8d4df406c5c315c784de8d92fdc761e49c6791dd
BoD <BoD@JRAF.org>
2020-11-06 11:22:30 +0100

    Update coroutines to 1.4.1

--- buildSrc/src/main/kotlin/Globals.kt
+++ buildSrc/src/main/kotlin/Globals.kt
@@ -4,6 +4,6 @@ object Versions {
     const val BEN_MANES_VERSIONS_PLUGIN = "0.33.0"
     const val BINTRAY_PUBLISH_PLUGIN = "1.8.5"
     const val KOTLIN = "1.4.10"
-    const val COROUTINES = "1.4.0"
+    const val COROUTINES = "1.4.1"
     const val ANDROIDX_LIFECYCLE_LIVEDATA = "2.2.0"
 }
